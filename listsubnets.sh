f7daa34fd0d12820ebe425230ecd85b90c02bbcc
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-13 15:13:04 +0200

    Update listsubnets.sh

--- listsubnets.sh
+++ listsubnets.sh
@@ -9,8 +9,9 @@ subnetsocids=$(cat subnets-$region.log | jq -r '.data.items[] | ."identifier"')
 for snid in $subnetsocids; do oci network subnet get --subnet-id $snid --region $region >> subnetdetails-$region.log; done
 cat subnetdetails-$region.log | jq -r '.[] | [."freeform-tags"."lm", ."id", ."vcn-id", ."display-name", ."subnet-domain-name", ."prohibit-public-ip-on-vnic", ."cidr-block"]| @csv ' > subnetdetails-$region.csv
 
-echo "[."freeform-tags"."lm", ."id", ."vcn-id", ."display-name", ."subnet-domain-name", ."prohibit-public-ip-on-vnic", ."cidr-block"]" > subnetdetails-global.csv
+echo "[."freeform-tags"."lm", ."id", ."vcn-id", ."display-name", ."subnet-domain-name", ."prohibit-public-ip-on-vnic", ."cidr-block"]" >> subnetdetails-global.csv
 cat subnetdetails-$region.csv >> subnetdetails-global.csv
-rm -f subnets-$region.log
+
 rm -f subnetdetails-$region.log
 rm -f subnetdetails-$region.csv
+rm -f subnets-$region.log
